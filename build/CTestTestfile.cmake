de52fe07cb3cbcc6b99491b8ffcef505df8774e5
lh <2370547972@qq.com>
2026-05-29 18:33:00 +0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/lh/Code/2026.5.28/GLIN
+# Build directory: /home/lh/Code/2026.5.28/GLIN/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_glin "/home/lh/Code/2026.5.28/GLIN/build/test_glin")
+set_tests_properties(test_glin PROPERTIES  ENVIRONMENT "LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/home/lh/anaconda3/lib:/usr/local/libtorch/lib:/usr/local/libtorch/lib:/usr/local/libtorch/lib:/usr/local/geos/lib:/usr/local/lib:/usr/local/libtorch/lib:/usr/local/libtorch/lib:" _BACKTRACE_TRIPLES "/home/lh/Code/2026.5.28/GLIN/CMakeLists.txt;37;add_test;/home/lh/Code/2026.5.28/GLIN/CMakeLists.txt;0;")
+add_test(test_glin_piece "/home/lh/Code/2026.5.28/GLIN/build/test_glin_piece")
+set_tests_properties(test_glin_piece PROPERTIES  ENVIRONMENT "LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/home/lh/anaconda3/lib:/usr/local/libtorch/lib:/usr/local/libtorch/lib:/usr/local/libtorch/lib:/usr/local/geos/lib:/usr/local/lib:/usr/local/libtorch/lib:/usr/local/libtorch/lib:" _BACKTRACE_TRIPLES "/home/lh/Code/2026.5.28/GLIN/CMakeLists.txt;38;add_test;/home/lh/Code/2026.5.28/GLIN/CMakeLists.txt;0;")
